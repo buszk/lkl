cdaa2dfb90aa2df6a576d3e173406f767b3deefd
Zekun Shen <bruceshenzk@gmail.com>
2022-03-11 16:17:07 -0500

    test

--- tools/lkl/instrumentation/test.c
+++ tools/lkl/instrumentation/test.c
@@ -2,12 +2,19 @@ int func(void) {
   return 0;
 }
 
+
 int main(void) {
   int err;
   err = func();
 
   if (err)
     goto err_out;
+
+  err = func();
+  if (err)
+    return err;
+    
+  err = func();
 err_out:
   return err;
 }
