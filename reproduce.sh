dee7e1785d88a0bb0a82f63a2fb994793ce740d3
Zekun Shen <bruceshenzk@gmail.com>
2022-03-26 12:20:55 -0400

    fix reproduce script with new binary parameter

--- reproduce.sh
+++ reproduce.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ -z $1 ];then
-    echo "$0 <crashes/hangs/queue>"
+if [ -z $2 ];then
+    echo "$0 <target> <crashes/hangs/queue>"
     exit
 fi
 
 echo "reproduce log:" > reproduce.log
-for f in ../AFLplusplus/output/default/$1/*; do
+for f in ../AFLplusplus/output/default/$2/*; do
     echo $f >> reproduce.log
-    (/bin/sh -c "time tools/lkl/harness/afl-harness $f") >> reproduce.log 2>&1
+    (/bin/sh -c "time tools/lkl/harness/afl-harness $1 $f") >> reproduce.log 2>&1
     code=$?
     if [ $code -gt 1 ];then
         echo "non-zero exit code: $code" >> reproduce.log
