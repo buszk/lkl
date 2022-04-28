0484fc0f9c530a520fd5df2598c0c0a113806805
Zekun Shen <bruceshenzk@gmail.com>
2022-04-28 12:06:08 -0400

    update script to generate CONFIG

--- scripts/parse_mf.py
+++ scripts/parse_mf.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 
+from mmap import mmap, ACCESS_READ
 import os
 import re
 import sys
 import glob
+from os.path import join, exists, dirname, basename, isdir
 
 
 def build_objmap(mf):
@@ -52,7 +54,7 @@ def build_objmap(mf):
     return confmap
 
 def build_confmap(cf):
-    if not os.path.exists(cf):
+    if not exists(cf):
         return {}
 
     with open(cf, 'r') as fd:
@@ -62,40 +64,67 @@ def build_confmap(cf):
     for line in data.split('\n'):
         if line.startswith("config"):
             conf = line.split()[1]
+            confmap[conf] = []
         elif line.strip().startswith("depends on"):
             confmap[conf] = line.split()[2:]
     #print(confmap)
     return confmap
 
 
-wordlist = [
-        "pci",
-        ]
-match = re.compile("|".join(wordlist))
-def interesting(key, dname):
-    if dname in key:
-        return True
-
-    if match.match(key):
-        return True
-
-    return False
+def files_have_keyword(dir):
+    files = []
+    for f in glob.glob(join(dir, "**")):
+        if isdir(f):
+            continue
+        with open(f, 'rb', 0) as file, mmap(file.fileno(), 0, access=ACCESS_READ) as s:
+            if s.find(b'USB_DEVICE') != -1:
+                files.append(basename(f))
+            elif s .find(b'PCI_DEVICE') != -1:
+                files.append(basename(f))
+    return files
 
 
 def scrape_dir(path):
-    for mf in glob.glob(os.path.join(path, "**", "Makefile"), recursive=True):
-        objmap = build_objmap(mf)
-        confmap = build_confmap(os.path.join(os.path.dirname(mf), "Kconfig"))
-        for obj in objmap:
-            if interesting(obj, os.path.basename(os.path.dirname(mf))):
-                conf = objmap[obj][0]
-                while not conf.startswith("$("):
-                    conf = objmap[conf][0]
-                conf = conf[2+len("CONFIG_"):-1]
-                print(mf)
-                print(conf)
-                if conf in confmap:
-                    print(confmap[conf])
+    config_match = re.compile(r"\$\((CONFIG_[A-Za-z0-9_]*?)\)")
+    allconfigs = set()
+    alldeps = set()
+    for mf in glob.glob(join(path, "**", "Makefile"), recursive=True):
+        confmap = build_confmap(join(dirname(mf), "Kconfig"))
+        files = files_have_keyword(dirname(mf))
+        if not files:
+            continue
+
+        # Proceed only when any file in driver directory contains keywords
+        instrumented = False
+        configs = set()
+        with open(mf, 'r') as file:
+            for line in file.readlines():
+                [configs.add(conf) for conf in config_match.findall(line)]
+                instrumented = instrumented or "AFLCOV" in line
+        configs = configs.intersection([f"CONFIG_{x}" for x in confmap.keys()])
+        for conf in configs:
+            alldeps.update(confmap[conf.replace("CONFIG_", "")] if conf.replace("CONFIG_", "") in confmap else set())
+        if configs:
+            print(mf)
+            print(configs)
+            allconfigs.update(configs)
+        # instrument AFLCOV to driver Makefile
+        if not instrumented:
+            with open(mf, 'r') as contents:
+                save = contents.read()
+                line1 = save.split('\n')[0]
+                save = save[len(line1)+1:]
+            with open(mf, 'w') as contents:
+                contents.write(f"{line1}\nAFLCOV := y\n")
+            with open(mf, 'a') as contents:
+                contents.write(save)
+                
+    # Print results
+    print()
+    [print(f"deps on {conf}") for conf in alldeps]
+    print()
+    [print(f"{conf}=y") for conf in allconfigs]
+
 
 if __name__== '__main__':
     testfile = sys.argv[1]
